019ba3f55e09212ee65083d3daf776bbc53ec13d
Natalia Castro <nat.alvz@gmail.com>
2018-06-04 20:25:05 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_accessor :name, :posts
   
-  @@all = []
+  @@all_posts = []
   
   def initialize(name)
     @name = name
@@ -11,19 +11,19 @@ class Author
   def add_post(post)
     post.author = self
     @posts << post
-    @@all << post 
+    @@all_posts << post 
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
     @posts << post
-    @@all << post
+    @@all_posts << post
   end
   
   
   def self.post_count
-    @@all.count 
+    @@all_posts.count 
   end
 
     
